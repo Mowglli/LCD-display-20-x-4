5021daeff3262bb104b6c846da0673505cb22f03
Jesper Egsgaard Nielsen <55810497+Mowglli@users.noreply.github.com>
2022-09-06 10:19:30 +0200

    Update lcd_display_20x4.ino

--- lcd_display_20x4.ino
+++ lcd_display_20x4.ino
@@ -11,9 +11,6 @@
 
 LiquidCrystal_I2C lcd(ADDRESS, CHARS, LINES);
 
-char Buffer[20];
-int Seconds;
-
 void setup() {
 
   Serial.begin(9600);
@@ -21,47 +18,23 @@ void setup() {
   lcd.begin(CHARS, LINES);
   lcd.init();
   lcd.backlight();
-  Seconds = 0;
+
+
+  lcd.setCursor(0,0);           
+  lcd.print("Hello!");
+
+  lcd.setCursor(0,1);             
+  lcd.print("Funny!");
+
+  lcd.setCursor(3,2);            
+  lcd.print("Enjoy!");
+
+  lcd.setCursor(8,3);             
+  lcd.print("Arduino!");
 
 }
 
 void loop() {
-
-#if LINES == 2
-
-  lcd.setCursor(1, 0);
-  lcd.print("Demo program");
-  lcd.setCursor(0, 1);
-  lcd.print("www.eBits.dk");
-
-#elif LINES == 4
-
-  lcd.setCursor(3, 0);
-  lcd.print("Demo program");
-
-  Seconds = int(millis() / 1000);
-  while (Seconds >= 9999) {
-     Seconds = Seconds - 9999;
-  }
-  
-  sprintf(Buffer, "--==[%04d]==--", Seconds);
-
-  lcd.setCursor(3, 1);
-  lcd.print(Buffer);
-
-  lcd.setCursor(2, 2);
-  lcd.print("LCD char display");
-  
-  lcd.setCursor(2, 3);
-  lcd.print("www.eBits.dk");
-
-#else
-
-  lcd.setCursor(0, 0);
-  lcd.print("ERROR!");
-
-#endif
-
-  delay(100);
+          
 
 }
